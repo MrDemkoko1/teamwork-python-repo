a65ccbe4e8a239006e11bfbdf29fe48e973af805
Demian Skakun <demian.skakun@gmail.com>
2024-11-06 12:35:58 +0200

    I have changed the code after code review

--- src/lab4.py
+++ src/lab4.py
@@ -12,6 +12,7 @@ class Farm:
 
     def __del__(self):
         print('Farm has been deleted')
+        del self
 
     def get_location(self):
         return self.__location
@@ -35,10 +36,10 @@ class Farm:
         return 0
 
     def __str__(self):
-        return f"Location: {self.__location}, Animals: {self.__animals_qty}, Fan Power: {self.__vent_power}W"
+        return f"Location: {self.__location}, Animals: {self.__animals_qty}, Amount of milk: {self.__amount_of_milk}, Fan Power: {self.__vent_power}W"
 
     def __repr__(self):
-        return f"Farm(location='{self.__location}', num_animals={self.__animals_qty}, fan_power={self.__vent_power}"
+        return f"Farm(location='{self.__location}', num_animals={self.__animals_qty}, amount_of_milk={self.__amount_of_milk}, fan_power={self.__vent_power})"
 
 
 def main():
@@ -53,7 +54,7 @@ def main():
     farms = [farm1, farm2, farm3]
 
     highest_efficiency = 0
-    most_efficient_farm = farms[0]
+    most_efficient_farm = None
 
     for farm in farms:
         if farm.efficiency() > highest_efficiency:
